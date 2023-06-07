ea5ac3031c4f2772c77954c4362e235a49933f25
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2023-06-07 22:29:48 +0200

    added shazam and billboard tracks data

---
+++ csv_converter.py
@@ -0,0 +1,18 @@
+import csv
+import json
+
+def csv_to_json(csv_file, json_file):
+    data = []
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        csv_data = csv.DictReader(file)
+        for row in csv_data:
+            data.append(row)
+
+    with open(json_file, 'w') as file:
+        json.dump(data, file, indent=4)
+
+# Usage example
+csv_file = 'Shazam_Top_200_Global_Chart.csv'
+json_file = 'shazam_track_data.json'
+csv_to_json(csv_file, json_file)
+
