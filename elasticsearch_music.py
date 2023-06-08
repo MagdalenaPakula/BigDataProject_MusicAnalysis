aaf015a4220ce78e0d7f9cc24405c06ef4a264f1
olabanasiak <234750@edu.p.lodz.pl>
2023-06-08 21:34:51 +0200

    analysis with kibana

--- elasticsearch_music.py
+++ elasticsearch_music.py
@@ -22,3 +22,59 @@ res = es.search(index='music_data', size=5)
 print("Indexed Documents:")
 for hit in res['hits']['hits']:
     print(hit['_source'])
+
+# Example: Count the number of tracks by artist genre
+agg_query = {
+    "size": 0,
+    "aggs": {
+        "genre_count": {
+            "terms": {
+                "field": "Artist Genres.keyword",
+                "size": 10
+            }
+        }
+    }
+}
+
+# Perform the aggregation query
+result = es.search(index='music_data', body=agg_query)
+
+# Extract the genre counts from the result
+genre_counts = result["aggregations"]["genre_count"]["buckets"]
+
+# Print the genre counts
+print("Genre Counts:")
+for genre in genre_counts:
+    print(f"{genre['key']}: {genre['doc_count']}")
+
+# Example: Visualize the track popularity over time
+date_agg_query = {
+    "size": 0,
+    "aggs": {
+        "track_popularity_over_time": {
+            "date_histogram": {
+                "field": "Release Date",
+                "calendar_interval": "month"
+            },
+            "aggs": {
+                "average_popularity": {
+                    "avg": {
+                        "field": "Track Popularity"
+                    }
+                }
+            }
+        }
+    }
+}
+
+# Perform the date aggregation query
+date_result = es.search(index='music_data', body=date_agg_query)
+
+# Extract the date histogram and average popularity from the result
+date_histogram = date_result["aggregations"]["track_popularity_over_time"]["buckets"]
+popularity_over_time = [(entry["key_as_string"], entry["average_popularity"]["value"]) for entry in date_histogram]
+
+# Print the track popularity over time
+print("Track Popularity Over Time:")
+for entry in popularity_over_time:
+    print(f"{entry[0]}: {entry[1]}")
