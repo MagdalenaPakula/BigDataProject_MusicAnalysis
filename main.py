7c61154654c8120e201ea2befe133d6c91dcd164
olabanasiak <234750@edu.p.lodz.pl>
2023-06-09 00:40:22 +0200

    tkinter some starter gui for main

---
+++ main.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from elasticsearch import Elasticsearch
+import pyarrow.parquet as pq
+import pandas as pd
+
+# Function to handle button click and perform the desired actions
+def run_program():
+    # Get the values from the input fields
+    parquet_file_path = parquet_entry.get()
+    elasticsearch_host = elasticsearch_entry.get()
+    index_name = index_entry.get()
+    query = query_entry.get()
+    output_file_path = output_entry.get()
+
+    # Load the formatted data from the Parquet file
+    parquet_data = pq.read_table(parquet_file_path)
+    formatted_df = parquet_data.to_pandas()
+
+    # Create an Elasticsearch instance
+    es = Elasticsearch(elasticsearch_host)
+
+    # Convert DataFrame records to a list of dictionaries
+    documents = formatted_df.to_dict(orient='records')
+
+    # Index each document
+    for doc in documents:
+        es.index(index=index_name, body=doc)
+
+    # Perform additional actions based on the provided input
+    # ...
+
+    # Show a success message
+    messagebox.showinfo("Success", "Program completed successfully.")
+
+# Create the main window
+window = tk.Tk()
+window.title("Music Data Analysis")
+window.geometry("400x300")
+
+# Create labels and entry fields for each input
+parquet_label = tk.Label(window, text="Parquet File:")
+parquet_label.pack()
+parquet_entry = tk.Entry(window)
+parquet_entry.pack()
+
+elasticsearch_label = tk.Label(window, text="Elasticsearch Host:")
+elasticsearch_label.pack()
+elasticsearch_entry = tk.Entry(window)
+elasticsearch_entry.pack()
+
+index_label = tk.Label(window, text="Index Name:")
+index_label.pack()
+index_entry = tk.Entry(window)
+index_entry.pack()
+
+query_label = tk.Label(window, text="Query:")
+query_label.pack()
+query_entry = tk.Entry(window)
+query_entry.pack()
+
+output_label = tk.Label(window, text="Output File:")
+output_label.pack()
+output_entry = tk.Entry(window)
+output_entry.pack()
+
+# Create a button to run the program
+run_button = tk.Button(window, text="Run", command=run_program)
+run_button.pack()
+
+# Start the GUI event loop
+window.mainloop()
