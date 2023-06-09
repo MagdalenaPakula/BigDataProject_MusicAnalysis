0b6dc9dca1ee4baa9cd432223988656514ddafc7
olabanasiak <234750@edu.p.lodz.pl>
2023-06-09 21:08:27 +0200

    better main

--- scripts/main.py
+++ scripts/main.py
@@ -10,40 +10,38 @@ def load_parquet_file():
     parquet_entry.delete(0, tk.END)
     parquet_entry.insert(tk.END, file_path)
 
-def run_program():
-    # Get the values from the input fields
+def ingest_data():
     parquet_file_path = parquet_entry.get()
+    # Add your code to ingest the data here
+
+def transform_data():
+    # Add your code to transform the data here
+    pass
+
+def combine_data():
+    # Add your code to combine the data here
+    pass
+
+def index_data():
     elasticsearch_host = elasticsearch_entry.get()
     index_name = index_entry.get()
-    query = query_entry.get()
-    output_file_path = output_entry.get()
+    # Add your code to index the data in Elasticsearch here
 
-    # Load the formatted data from the Parquet file
-    parquet_data = pq.read_table(parquet_file_path)
-    formatted_df = parquet_data.to_pandas()
-
-    # Create an Elasticsearch instance
-    es = Elasticsearch(elasticsearch_host)
-
-    # Convert DataFrame records to a list of dictionaries
-    documents = formatted_df.to_dict(orient='records')
-
-    # Index each document
-    for doc in documents:
-        es.index(index=index_name, body=doc)
-
-    # Perform additional actions based on the provided input
-    # ...
-
-    # Show a success message
-    messagebox.showinfo("Success", "Program completed successfully.")
+def execute_data_pipeline():
+    try:
+        ingest_data()
+        transform_data()
+        combine_data()
+        index_data()
+        messagebox.showinfo("Success", "Data pipeline executed successfully.")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
 # Create the main window
 window = tk.Tk()
 window.title("Music Data Analysis")
 window.geometry("400x300")
 
-# Create labels and entry fields for each input
 parquet_label = tk.Label(window, text="Parquet File:")
 parquet_label.pack()
 parquet_entry = tk.Entry(window)
@@ -62,19 +60,7 @@ index_label.pack()
 index_entry = tk.Entry(window)
 index_entry.pack()
 
-query_label = tk.Label(window, text="Query:")
-query_label.pack()
-query_entry = tk.Entry(window)
-query_entry.pack()
-
-output_label = tk.Label(window, text="Output File:")
-output_label.pack()
-output_entry = tk.Entry(window)
-output_entry.pack()
-
-# Create a button to run the program
-run_button = tk.Button(window, text="Run", command=run_program)
+run_button = tk.Button(window, text="Run", command=execute_data_pipeline)
 run_button.pack()
 
-# Start the GUI event loop
 window.mainloop()
