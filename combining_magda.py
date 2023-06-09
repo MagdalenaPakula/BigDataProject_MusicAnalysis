4e220920a80d2448a0c8e744031aad9e98f702ce
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2023-06-09 18:46:50 +0200

    combining 3 datas

--- combining_magda.py
+++ combining_magda.py
@@ -8,8 +8,22 @@ with open('formatted_files/formatted_billboard_track_data.json') as file:
 with open('formatted_files/formatted_spotify_data.json') as file:
     data2 = json.load(file)
 
+# Load the third JSON data
+with open('formatted_files/formatted_lastfm_track_data.json') as file:
+    data3 = json.load(file)
+
 # Combine the data based on the 'Track Name' key
 combined_data = {}
+
+# Function to merge two dictionaries
+def merge_dicts(dict1, dict2):
+    result = dict1.copy()
+    for key, value in dict2.items():
+        if key in result and isinstance(result[key], dict) and isinstance(value, dict):
+            result[key] = merge_dicts(result[key], value)
+        else:
+            result[key] = value
+    return result
 
 # Iterate over the first dataset and add its values to the combined dataset
 for item in data1:
@@ -20,7 +34,15 @@ for item in data1:
 for item in data2:
     track_name = item['Track Name']
     if track_name in combined_data:
-        combined_data[track_name].update(item)
+        combined_data[track_name] = merge_dicts(combined_data[track_name], item)
+    else:
+        combined_data[track_name] = item
+
+# Iterate over the third dataset and merge its values with the combined dataset
+for item in data3:
+    track_name = item['name']
+    if track_name in combined_data:
+        combined_data[track_name] = merge_dicts(combined_data[track_name], item)
     else:
         combined_data[track_name] = item
 
