75df078cbda65851f6667a522881316ee6686ed0
olabanasiak <234750@edu.p.lodz.pl>
2023-06-09 20:58:59 +0200

    formatted scripts into function

--- scripts/formatting/formatted_billboard.py
+++ scripts/formatting/formatted_billboard.py
@@ -1,40 +1,85 @@
+# import json
+# import pandas as pd
+# import pyarrow.parquet as pq
+
+# # Load the billboard track data from the JSON
+# with open("../../data/raw/billboard_track_data.json", "r") as json_file:
+#     billboard_track_data = json.load(json_file)
+
+# # Extract the required fields and create a new list of dictionaries
+# formatted_data = []
+# for track in billboard_track_data["data"]:
+#     formatted_track = {
+#         "Track Name": track["name"],
+#         "Artist Name": track["artist"],
+#         "Image": track["image"],
+#         "Rank": track["rank"],
+#         "Last Week Rank": track["last_week_rank"],
+#         "Peak Rank": track["peak_rank"],
+#         "Weeks on Chart": track["weeks_on_chart"]
+#     }
+#     formatted_data.append(formatted_track)
+
+# # Create a pandas DataFrame from the formatted data
+# formatted_df = pd.DataFrame(formatted_data)
+
+# # Save the formatted data to a JSON file
+# formatted_df.to_json("formatted_billboard_track_data.json", orient="records", indent=4)
+
+# # Display the formatted data
+# # print(formatted_df)
+
+# # Save the formatted data to a Parquet file
+# formatted_df.to_parquet("formatted_billboard_track_data.parquet")
+
+# # Read the Parquet file
+# parquet_data = pq.read_table("formatted_billboard_track_data.parquet")
+# parquet_df = parquet_data.to_pandas()
+
+# # Display the content of the Parquet file
+# print(parquet_df)
+
 import json
 import pandas as pd
 import pyarrow.parquet as pq
 
-# Load the billboard track data from the JSON
-with open("../../data/raw/billboard_track_data.json", "r") as json_file:
-    billboard_track_data = json.load(json_file)
+def format_billboard_track_data(input_json, output_json, output_parquet):
+    # Load the billboard track data from the JSON
+    with open(input_json, "r") as json_file:
+        billboard_track_data = json.load(json_file)
 
-# Extract the required fields and create a new list of dictionaries
-formatted_data = []
-for track in billboard_track_data["data"]:
-    formatted_track = {
-        "Track Name": track["name"],
-        "Artist Name": track["artist"],
-        "Image": track["image"],
-        "Rank": track["rank"],
-        "Last Week Rank": track["last_week_rank"],
-        "Peak Rank": track["peak_rank"],
-        "Weeks on Chart": track["weeks_on_chart"]
-    }
-    formatted_data.append(formatted_track)
+    # Extract the required fields and create a new list of dictionaries
+    formatted_data = []
+    for track in billboard_track_data["data"]:
+        formatted_track = {
+            "Track Name": track["name"],
+            "Artist Name": track["artist"],
+            "Image": track["image"],
+            "Rank": track["rank"],
+            "Last Week Rank": track["last_week_rank"],
+            "Peak Rank": track["peak_rank"],
+            "Weeks on Chart": track["weeks_on_chart"]
+        }
+        formatted_data.append(formatted_track)
 
-# Create a pandas DataFrame from the formatted data
-formatted_df = pd.DataFrame(formatted_data)
+    # Create a pandas DataFrame from the formatted data
+    formatted_df = pd.DataFrame(formatted_data)
 
-# Save the formatted data to a JSON file
-formatted_df.to_json("formatted_billboard_track_data.json", orient="records", indent=4)
+    # Save the formatted data to a JSON file
+    formatted_df.to_json(output_json, orient="records", indent=4)
 
-# Display the formatted data
-# print(formatted_df)
+    # Save the formatted data to a Parquet file
+    formatted_df.to_parquet(output_parquet)
 
-# Save the formatted data to a Parquet file
-formatted_df.to_parquet("formatted_billboard_track_data.parquet")
+    # Read the Parquet file
+    parquet_data = pq.read_table(output_parquet)
+    parquet_df = parquet_data.to_pandas()
 
-# Read the Parquet file
-parquet_data = pq.read_table("formatted_billboard_track_data.parquet")
-parquet_df = parquet_data.to_pandas()
+    # Display the content of the Parquet file
+    print(parquet_df)
 
-# Display the content of the Parquet file
-print(parquet_df)
+# Example usage
+input_json = "../../data/raw/billboard_track_data.json"
+output_json = "formatted_billboard_track_data.json"
+output_parquet = "formatted_billboard_track_data.parquet"
+format_billboard_track_data(input_json, output_json, output_parquet)
--- scripts/formatting/formatter.py
+++ scripts/formatting/formatter.py
@@ -1,23 +1,53 @@
+# import json
+# import pandas as pd
+
+# # Load the JSON data
+# with open('../../data/raw/shazam_track_data.json', 'r') as file:
+#     data = json.load(file)
+
+# # Extract the relevant information and convert it into a list of dictionaries
+# formatted_data = []
+# header = data[1]['null']
+# for entry in data[2:]:
+#     track_info = {
+#         'Track Name': entry['null'][1],
+#         'Artist Name': entry['null'][0],
+#         'Rank': int(entry['\ufeffShazam Top 200 Global Chart: The most Shazamed tracks in the world'])
+#     }
+#     formatted_data.append(track_info)
+
+# # Convert the list of dictionaries into a pandas DataFrame
+# df = pd.DataFrame(formatted_data)
+
+# # Save the formatted data to a new file in Feather format
+# df.to_feather('formatted/shazam_formatted.feather')
+
 import json
 import pandas as pd
 
-# Load the JSON data
-with open('../../data/raw/shazam_track_data.json', 'r') as file:
-    data = json.load(file)
+def format_shazam_track_data(input_json, output_feather):
+    # Load the JSON data
+    with open(input_json, 'r') as file:
+        data = json.load(file)
 
-# Extract the relevant information and convert it into a list of dictionaries
-formatted_data = []
-header = data[1]['null']
-for entry in data[2:]:
-    track_info = {
-        'Track Name': entry['null'][1],
-        'Artist Name': entry['null'][0],
-        'Rank': int(entry['\ufeffShazam Top 200 Global Chart: The most Shazamed tracks in the world'])
-    }
-    formatted_data.append(track_info)
+    # Extract the relevant information and convert it into a list of dictionaries
+    formatted_data = []
+    header = data[1]['null']
+    for entry in data[2:]:
+        track_info = {
+            'Track Name': entry['null'][1],
+            'Artist Name': entry['null'][0],
+            'Rank': int(entry['\ufeffShazam Top 200 Global Chart: The most Shazamed tracks in the world'])
+        }
+        formatted_data.append(track_info)
 
-# Convert the list of dictionaries into a pandas DataFrame
-df = pd.DataFrame(formatted_data)
+    # Convert the list of dictionaries into a pandas DataFrame
+    df = pd.DataFrame(formatted_data)
 
-# Save the formatted data to a new file in Feather format
-df.to_feather('formatted/shazam_formatted.feather')
+    # Save the formatted data to a new file in Feather format
+    df.to_feather(output_feather)
+
+# Example usage
+input_json = '../../data/raw/shazam_track_data.json'
+output_feather = 'formatted/shazam_formatted.feather'
+format_shazam_track_data(input_json, output_feather)
