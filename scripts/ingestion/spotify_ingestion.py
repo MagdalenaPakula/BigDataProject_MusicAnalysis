1f4010f2d08498d084ed3d1a1edf2afecdb373da
olabanasiak <234750@edu.p.lodz.pl>
2023-06-09 22:35:26 +0200

    spotify kafka

--- scripts/ingestion/spotify_ingestion.py
+++ scripts/ingestion/spotify_ingestion.py
@@ -88,6 +88,23 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 import json
 import time
+from kafka import KafkaProducer
+
+def ingest_data_to_kafka(data):
+    # Configure Kafka producer
+    bootstrap_servers = 'localhost:9092'
+    topic = 'test'
+
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
+                             value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+
+    # Ingest each track data into Kafka
+    for track in data:
+        producer.send(topic, value=track)
+        time.sleep(1)  # Pause execution for 1 second between each ingestion
+
+    producer.flush()
+    producer.close()
 
 def scrape_spotify_playlist():
     # CLIENT AUTHORISATION
@@ -102,65 +119,70 @@ def scrape_spotify_playlist():
     playlist_URI = playlist_link.split("/")[-1].split("?")[0]
     track_uris = [x["track"]["uri"] for x in sp.playlist_items(playlist_URI)["items"]]
 
-    # Create a list to store the track data
-    track_data = []
+    while True:
+        # Create a list to store the track data
+        track_data = []
 
-    for track in sp.playlist_items(playlist_URI)["items"]:
-        # URI
-        track_uri = track["track"]["uri"]
+        for track in sp.playlist_items(playlist_URI)["items"]:
+            # URI
+            track_uri = track["track"]["uri"]
 
-        # Track name
-        track_name = track["track"]["name"]
+            # Track name
+            track_name = track["track"]["name"]
 
-        # Main Artist
-        artist_uri = track["track"]["artists"][0]["uri"]
-        artist_info = sp.artist(artist_uri)
+            # Main Artist
+            artist_uri = track["track"]["artists"][0]["uri"]
+            artist_info = sp.artist(artist_uri)
 
-        # Name, popularity, genre
-        artist_name = artist_info["name"]
-        artist_pop = artist_info["popularity"]
-        artist_genres = artist_info["genres"]
+            # Name, popularity, genre
+            artist_name = artist_info["name"]
+            artist_pop = artist_info["popularity"]
+            artist_genres = artist_info["genres"]
 
-        # Album
-        album = track["track"]["album"]["name"]
+            # Album
+            album = track["track"]["album"]["name"]
 
-        # Popularity of the track
-        track_pop = track["track"]["popularity"]
+            # Popularity of the track
+            track_pop = track["track"]["popularity"]
 
-        # Audio features of the track
-        audio_features = sp.audio_features(track_uri)[0]
+            # Audio features of the track
+            audio_features = sp.audio_features(track_uri)[0]
 
-        # Duration of the track
-        duration_ms = track["track"]["duration_ms"]
-        duration_sec = duration_ms / 1000
+            # Duration of the track
+            duration_ms = track["track"]["duration_ms"]
+            duration_sec = duration_ms / 1000
 
-        # Release date of the track
-        release_date = track["track"]["album"]["release_date"]
+            # Release date of the track
+            release_date = track["track"]["album"]["release_date"]
 
-        # Create a dictionary with the track data
-        track_dict = {
-            "Track Name": track_name,
-            "Artist Name": artist_name,
-            "Artist Popularity": artist_pop,
-            "Artist Genres": artist_genres,
-            "Album": album,
-            "Track Popularity": track_pop,
-            "Release Date": release_date,
-            "Duration (sec)": duration_sec,
-            "Audio Features": audio_features
-        }
+            # Create a dictionary with the track data
+            track_dict = {
+                "Track Name": track_name,
+                "Artist Name": artist_name,
+                "Artist Popularity": artist_pop,
+                "Artist Genres": artist_genres,
+                "Album": album,
+                "Track Popularity": track_pop,
+                "Release Date": release_date,
+                "Duration (sec)": duration_sec,
+                "Audio Features": audio_features
+            }
 
-        # Add the track data to the list
-        track_data.append(track_dict)
+            # Add the track data to the list
+            track_data.append(track_dict)
 
-        # Pause execution for a second to avoid rate limiting
-        time.sleep(1)
+            # Pause execution for a second to avoid rate limiting
+            time.sleep(1)
 
-    # Save the track data to a JSON file
-    with open("../../data/raw/spotify_track_data.json", "w") as json_file:
-        json.dump(track_data, json_file)
+        # Save the track data to a JSON file
+        with open('spotify_track_data.json', 'w') as file:
+            json.dump(track_data, file)
 
-    print("Track data saved to spotify_track_data.json")
+        # Ingest the track data into Kafka
+        ingest_data_to_kafka(track_data)
+
+        print("Track data ingested into Kafka")
+        time.sleep(300)  # Pause execution for 5 minutes before scraping again
 
 # Example usage
 scrape_spotify_playlist()
